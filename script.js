a39c01d7bc372ee225102076a3fc7c3decebf361
Nayeon Shin <nayeon.codes@gmail.com>
2022-03-02 16:16:01 -0500

    feat: Delete data and generate random ids with `nanoid`

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import lodash from "lodash";
 import { Low, JSONFile } from "lowdb";
 
+import { nanoid } from "nanoid";
+
 // Extend Low class with a new `chain` field
 class LowWithLodash extends Low {
   chain = lodash.chain(this).get("data");
@@ -10,43 +12,47 @@ const adapter = new JSONFile("db.json");
 const db = new LowWithLodash(adapter);
 await db.read();
 
-// // If file.json doesn't exist, db.data will be null
-// // Set default data
-// db.data = db.data || { authors: [], topics: [] }; // Node < v15.x
+/* ------------------------------ Create ------------------------------ */
+// If file.json doesn't exist, db.data will be null
+// Set default data
+db.data = db.data || { authors: [], topics: [] }; // Node < v15.x
 
-// const { authors } = db.data;
+const { authors } = db.data;
+const { topics } = db.data;
 
-// authors.push({
-//   id: 1,
-//   name: "Nayeon",
-//   title: "Aspiring back-end developer",
-// });
+authors.push({
+  id: 1,
+  name: "Nayeon",
+  title: "Aspiring back-end developer",
+});
 
-// const { topics } = db.data;
+const { topics } = db.data;
 
-// topics.push({
-//   id: 1,
-//   title: "lowdb",
-//   description: "lowdb is a simple-to-use local JSON database.",
-//   author: 1,
-// });
+topics.push({
+  id: 1,
+  title: "lowdb",
+  description: "lowdb is a simple-to-use local JSON database.",
+  author: 1,
+});
 
-// topics.push({
-//   id: 2,
-//   title: "MySQL",
-//   description: "MySQL is an open-source relational database management system.",
-//   author: 1,
-// });
+topics.push({
+  id: 2,
+  title: "MySQL",
+  description: "MySQL is an open-source relational database management system.",
+  author: 1,
+});
 
-// // Finally write db.data content to file
-// await db.write();
+// Finally write db.data content to file
+await db.write();
 
+/* ------------------------------ Read ------------------------------ */
 const lowdb = db.chain
   .get("topics")
   .find({ title: "lowdb", author: 1 })
   .value();
 console.log(lowdb);
 
+/* ------------------------------ Update ------------------------------ */
 db.chain
   .get("topics")
   .find({ id: 2 })
@@ -56,5 +62,26 @@ db.chain
       "MySQL is an open-source relational database management system. MariaDB is a community-developed, commercially supported fork of the MySQL relational database management system.",
   })
   .value();
+await db.write();
+
+/* ------------------------------ Delete ------------------------------ */
+db.chain.get("topics").remove({ id: 2 }).value();
+await db.write();
+
+/* ------------------------------ Random ids ------------------------------ */
+const nanoId = nanoid();
+authors.push({
+  id: nanoId,
+  name: "Nayoni",
+  title: "Nayeon's clone",
+});
+
+topics.push({
+  id: nanoid(),
+  title: "PostgreSQL",
+  description:
+    "PostgreSQL is a free and open-source relational database management system emphasizing extensibility and SQL compliance.",
+  author: nanoId,
+});
 
 await db.write();
